00f471b1379aae5f53eb5f567e5dd2b08f33eb53
thientt <pcking60@gmail.com>
2022-10-28 16:59:55 +0700

    initial_project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Page404 from "./pages/Page404";
+import { IntlProvider } from "react-intl";
+import { getDirection } from "./helpers/Utils";
+import { Suspense, useEffect } from "react";
+import { NotificationContainer } from "./components/common/react-notifications";
+import AppLocale from "./lang";
+function App({ locale }) {
+  const direction = getDirection();
+  const currentAppLocale = AppLocale["en"];
+  useEffect(() => {
+    if (direction.isRtl) {
+      document.body.classList.add("rtl");
+      document.body.classList.remove("ltr");
+    } else {
+      document.body.classList.add("ltr");
+      document.body.classList.remove("rtl");
+    }
+  }, [direction]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-100">
+      {/* <IntlProvider
+        locale={currentAppLocale.locale}
+        messages={currentAppLocale.messages}
+      > */}
+      <>
+        {/* <NotificationContainer /> */}
+
+        <Suspense fallback={<div className="loading" />}>
+          <Routes>
+            <Route path="/*" element={<Page404 />} />
+          </Routes>
+        </Suspense>
+      </>
+      {/* </IntlProvider> */}
     </div>
   );
 }
